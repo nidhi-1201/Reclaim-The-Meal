cdca017a410771e7236242dce98bb158a3376f5d
nidhi-1201 <pnidhi03@gmail.com>
2022-06-07 15:18:58 -0400

    Initial Commit

---
+++ src/main/java/com/ReclaimTheMeal/ReclaimTheMealApplication.java
@@ -0,0 +1,13 @@
+package com.ReclaimTheMeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclaimTheMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclaimTheMealApplication.class, args);
+	}
+
+}
