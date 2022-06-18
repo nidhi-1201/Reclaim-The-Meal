1701fef8a1f3589cdbad08ee82d44a968f7fd682
nidhi-1201 <pnidhi03@gmail.com>
2022-06-18 10:24:54 -0400

    Modified the files for error in user registration.

--- src/main/java/com/ReclaimTheMeal/ReclaimTheMealApplication.java
+++ src/main/java/com/ReclaimTheMeal/ReclaimTheMealApplication.java
@@ -2,7 +2,12 @@ package com.ReclaimTheMeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+<<<<<<< Updated upstream
+=======
+@Configuration
+>>>>>>> Stashed changes
 @SpringBootApplication
 public class ReclaimTheMealApplication {
 
