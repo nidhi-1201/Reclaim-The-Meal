3cbbfc6085005cb84c42e51986ff8ecd8fdf344a
parthvakil002 <107081639+parthvakil002@users.noreply.github.com>
2022-07-19 00:19:57 -0400

    Initial Commit

---
+++ src/test/java/com/ReclaimTheMeal/GrabTheMealv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parth.grabthemealv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabTheMealv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
