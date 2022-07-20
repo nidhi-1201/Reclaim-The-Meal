62e7f3aa79d09fa59bd2038a227142d5b96b6c61
riddhithakkar17 <thakkarriddhi999@gmail.com>
2022-07-19 23:52:30 -0400

    Files modified for cookie handling

--- src/main/java/com/ReclaimTheMeal/AppController.java
+++ src/main/java/com/ReclaimTheMeal/AppController.java
@@ -9,11 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Controller
 public class AppController {
 	@Autowired
+	HttpSession httpSession;
+
+	@Autowired
 	private UserRepository repo;
+
+	@Autowired
+	private RoleRepository rolesRepo;
+
 	@GetMapping("")
 	public String viewHomePage()
 	{
@@ -31,16 +43,51 @@ public class AppController {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 	    String encodedPassword = encoder.encode(user.getPassword());
 	    user.setPassword(encodedPassword);
-	    
+	    Role role = rolesRepo.getReferenceById(2L);
+	    user.addRole(role);
 	   repo.save(user);
+	   new EmailSend().send(user.getEmail());
 	     
 	    return "register_success";
 	}
+
 	@GetMapping("/list_users")
 	public String viewUsersList(Model model) {
 		List<User> listUsers = repo.findAll();
 	model.addAttribute("listUsers", listUsers);
 		return "users";
 	}
+
+	@GetMapping("/custom_login_url")
+	public String showLogin() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = "";
+		if (principal instanceof UserDetails) {
+		  username = ((UserDetails)principal).getUsername();
+		} else {
+		  username = principal.toString();
+		}
+
+		if( username!=null && !username.isEmpty() && !username.equals("anonymousUser")) {
+			return "redirect:/posts";
+			//return "login";
+
+		}
+		else {
+			return "login";
+		}
 	}
 
+	@GetMapping("/emailSend")
+	public String sendEmail() {
+		new EmailSend().send("thakka95@uwindsor.ca");
+		return "posts";
+	}
+
+	@GetMapping("/emailVerify/{code}")
+	public String verifyEmail(String code) {
+		new EmailVerify().verify(code, "thakka95@uwindsor.ca");
+		return "posts";
+	}
+
+}
