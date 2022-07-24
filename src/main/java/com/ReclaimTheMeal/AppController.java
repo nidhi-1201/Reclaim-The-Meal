0fc44239a5ef2a3d47ec1d6b889eda253b046af8
nidhi-1201 <pnidhi03@gmail.com>
2022-07-23 21:15:00 -0400

    Update profile and User delete funtionality added

--- src/main/java/com/ReclaimTheMeal/AppController.java
+++ src/main/java/com/ReclaimTheMeal/AppController.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,8 +27,16 @@ public class AppController {
 	private RoleRepository rolesRepo;
 
 	@GetMapping("")
-	public String viewHomePage()
+	public String viewHomePage(Model model)
 	{
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = "";
+		if (principal instanceof UserDetails) {
+		  username = ((UserDetails)principal).getUsername();
+		} else {
+		  username = principal.toString();
+		}
+        model.addAttribute("username", username);
 		return "index";
 	}
 	@GetMapping("/register")
@@ -43,9 +53,7 @@ public class AppController {
 	    user.setPassword(encodedPassword);
 	    Role role = rolesRepo.getReferenceById(2L);
 	    user.addRole(role);
-	   repo.save(user);
-	//    new EmailSend().send(user.getEmail());
-	     
+	  	repo.save(user);
 	    return "register_success";
 	}
 
@@ -67,25 +75,40 @@ public class AppController {
 		}
 
 		if( username!=null && !username.isEmpty() && !username.equals("anonymousUser")) {
-			return "redirect:/posts";
-			//return "login";
-
+			return "redirect:/";
 		}
 		else {
 			return "login";
 		}
 	}
 
-	@GetMapping("/emailSend")
-	public String sendEmail() {
-		new EmailSend().send("thakka95@uwindsor.ca");
-		return "posts";
-	}
+	@GetMapping("/users/update/{id}")
+    public String showFormtoUpdateUser(@PathVariable( value = "id") long id, Model model) {
+  
+        User user = repo.getReferenceById(id);
+        model.addAttribute("user", user);
+        return "usereditform";
+    }
 
-	@GetMapping("/emailVerify/{code}")
-	public String verifyEmail(String code) {
-		new EmailVerify().verify(code, "thakka95@uwindsor.ca");
-		return "posts";
-	}
+	@GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable (value = "id") long id) {
+  
+        this.repo.deleteById(id);
+        return "redirect:/";
+    }
 
+	@PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") User user) {
+        try {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String encodedPassword = encoder.encode(user.getPassword());
+			user.setPassword(encodedPassword);
+			Role role = rolesRepo.getReferenceById(2L);
+			user.addRole(role);
+			repo.save(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
 }
